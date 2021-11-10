7fc5a737875f5320de35ec7be083a46aa484cda6
Kathryn Reardon <kathryn.c.reardon@gmail.com>
2021-11-10 15:08:35 -0500

    Write the HeroDetailComponent template and add the Input hero property

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  @Input() hero?: Hero; // must be an Input property because the external HeroesComponent will bind to it
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
