406067eba6c94f93c58638eb1c49ed61820bfffe
Kathryn Reardon <kathryn.c.reardon@gmail.com>
2021-11-11 11:09:36 -0500

    Add a default route and add dashboard link to the shell

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; // Allows application to have routing functionality
 // Lines 4 and 5 give the Router somewhere to go once routes are configured
 import { HeroesComponent } from './heroes/heroes.component';
-import {DashboardComponent} from "./dashboard/dashboard.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 // Routes tell the Router which view to display when a user clicks a link or pastes a URL into the address bar
 const routes: Routes = [
   /* A typical Angular Route has two properties:
    *    path: a string that matches the URL in the browser address bar
    *    component: the component that the router should create when navigating to this route */
   { path: 'heroes', component: HeroesComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 // The @NgModule metadata initializes the router and starts it listening for browser location changes
 @NgModule({
