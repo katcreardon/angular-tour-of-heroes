20f85a0dd214d3e484d04948dc7a9f585dc52cde
Kathryn Reardon <kathryn.c.reardon@gmail.com>
2021-11-11 11:14:13 -0500

    Delete hero details from HeroesComponent and add a hero detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; // Allows application to have routing functionality
-// Lines 4 and 5 give the Router somewhere to go once routes are configured
+// Lines 4-6 give the Router somewhere to go once routes are configured
 import { HeroesComponent } from './heroes/heroes.component';
-import { DashboardComponent } from "./dashboard/dashboard.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 // Routes tell the Router which view to display when a user clicks a link or pastes a URL into the address bar
 const routes: Routes = [
   /* A typical Angular Route has two properties:
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent }, // : in the path indicates that :id is a placeholder for a specific hero id
 ];
 // The @NgModule metadata initializes the router and starts it listening for browser location changes
 @NgModule({
